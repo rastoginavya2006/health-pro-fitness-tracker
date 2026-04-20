c6e31790ea97c49f9bfd0090749599e7a8a4deff
Navya <rastoginavya2006@gmail.com>
2026-04-20 23:52:36 +0530

    Removed extra unrelated files

--- lib/screens/Notes/notes_screen.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class NotesScreen extends StatelessWidget {
-  const NotesScreen({super.key});
-  @override
-  Widget build(BuildContext context) => const Scaffold(body: Center(child: Text("Notes View")));
-}
